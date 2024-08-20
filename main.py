27497580d1461eb04bf02e1f24561bab3051ad39
Jason <feng_zixuan@126.com>
2024-08-20 17:10:43 +0800

    stock：获取标的的开盘，收盘，最高，最低价

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import datetime
 from stock.stock_base_class import stockBase
 from stock.stock_data_class import stockData
 import pandas as pd
 from WindPy import w
 
+TodayDate = datetime.datetime.today().strftime("%Y-%m-%d")
 
 stockBase = stockBase("000001", ".sz")
 #print(stockName.codeNumStr, stockName.exchangeTailStr)
@@ -10,13 +12,15 @@ stockBase = stockBase("000001", ".sz")
 
 stock = stockData()
 stock.stockName = stockBase.codeNameStr()
-
+stock.stockInfoDate = TodayDate
+ 
 w.start()
-w.isconnected()
 
-data = pd.DataFrame()
-data = w.wss(stock.stockName, "OPEN")
+stock.closePrice = w.wsd(stock.stockName, "close", "ED", stock.stockInfoDate, "PriceAdj=F", usedf=True)[1].iloc[0,0]
+stock.openPrice = w.wsd(stock.stockName, "open", "ED", stock.stockInfoDate, "PriceAdj=F", usedf=True)[1].iloc[0,0]
+stock.highestPrice = w.wsd(stock.stockName, "high", "ED", stock.stockInfoDate, "PriceAdj=F", usedf=True)[1].iloc[0,0]
+stock.lowestPrice = w.wsd(stock.stockName, "low", "ED", stock.stockInfoDate, "PriceAdj=F", usedf=True)[1].iloc[0,0]
 
-stock.openPrice = data.Data
+w.close()
 
-print(stock.openPrice)
+print(stock.lowestPrice, stock.highestPrice, stock.openPrice, stock.closePrice)
